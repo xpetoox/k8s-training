2ff176dbc13ddbd4560a08ec74db7485f17f81e9
xpetoox <felcan@drift-angle.sk>
2019-02-25 16:42:52 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,13 @@
 docker build -t playdude/multi-client:latest -t playdude/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
 docker build -t playdude/multi-server:latest -t playdude/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
 docker build -t playdude/multi-worker:latest -t playdude/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
-
 docker push playdude/multi-client:latest
 docker push playdude/multi-server:latest
 docker push playdude/multi-worker:latest
-
 docker push playdude/multi-client:$GIT_SHA
 docker push playdude/multi-server:$GIT_SHA
 docker push playdude/multi-worker:$GIT_SHA
-
 kubectl apply -f k8s
-
 kubectl set image deployments/client-deployment client=playdude/multi-client:$GIT_SHA
 kubectl set image deployments/server-deployment server=playdude/multi-server:$GIT_SHA
 kubectl set image deployments/worker-deployment worker=playdude/multi-worker:$GIT_SHA
